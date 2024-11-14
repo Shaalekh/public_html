7e9c8b036c83eea4152123efdd662b5c0dc4587a
Aalekh Kumar Sharma <shaalekh@gmail.com>
2024-11-14 19:42:13 +0530

    update php

--- upload.php
+++ upload.php
@@ -1,4 +1,8 @@
 <?php
+require 'src/Endroid/QrCode/QrCode.php';  // Include the QR code library
+
+use Endroid\QrCode\QrCode;
+
 $targetDir = "uploads/";
 if (!is_dir($targetDir)) {
     mkdir($targetDir, 0777, true);
@@ -14,7 +18,20 @@ if (isset($_FILES["fileToUpload"])) {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFilePath)) {
         // Generate a link to the file
         $fileUrl = "https://" . $_SERVER['SERVER_NAME'] . "/uploads/" . $uniqueName;
-        echo "File uploaded successfully! Download link: <a href='$fileUrl'>$fileUrl</a>";
+
+        // Generate QR code for the download link
+        $qrCode = new QrCode($fileUrl);
+        $qrCode->setSize(150);
+
+        // Save the QR code as an image file
+        $qrCodeImagePath = $targetDir . "qr_" . $uniqueName . ".png";
+        $qrCode->writeFile($qrCodeImagePath);
+
+        // Display success message with download link and QR code
+        echo "File uploaded successfully! <br>";
+        echo "Download link: <a href='$fileUrl'>$fileUrl</a><br>";
+        echo "Scan this QR code to download: <br>";
+        echo "<img src='$qrCodeImagePath' alt='QR Code for download link'>";
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
