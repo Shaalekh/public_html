82be2e3474ee0a946e99d67e000a7ed383745790
Aalekh Sharma <shaalekh@gmail.com>
2024-11-14 13:16:51 -0500

    lele re

--- upload.php
+++ upload.php
@@ -1,8 +1,4 @@
 <?php
-require 'src/Endroid/QrCode/QrCode.php';  // Include the QR code library
-
-use Endroid\QrCode\QrCode;
-
 $targetDir = "uploads/";
 if (!is_dir($targetDir)) {
     mkdir($targetDir, 0777, true);
@@ -16,22 +12,18 @@ if (isset($_FILES["fileToUpload"])) {
 
     // Move uploaded file to the target directory
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFilePath)) {
-        // Generate a link to the file
+        // Generate the download link
         $fileUrl = "https://" . $_SERVER['SERVER_NAME'] . "/uploads/" . $uniqueName;
-
-        // Generate QR code for the download link
-        $qrCode = new QrCode($fileUrl);
-        $qrCode->setSize(150);
-
-        // Save the QR code as an image file
-        $qrCodeImagePath = $targetDir . "qr_" . $uniqueName . ".png";
-        $qrCode->writeFile($qrCodeImagePath);
-
-        // Display success message with download link and QR code
-        echo "File uploaded successfully! <br>";
-        echo "Download link: <a href='$fileUrl'>$fileUrl</a><br>";
-        echo "Scan this QR code to download: <br>";
-        echo "<img src='$qrCodeImagePath' alt='QR Code for download link'>";
+        
+        // Create QR code URL (GoQR API)
+        $qrCodeUrl = "https://api.qrserver.com/v1/create-qr-code/?data=" . urlencode($fileUrl) . "&size=150x150&color=FFFFFF&bgcolor=000000&qzone=2";
+        
+        // Display success message and QR code
+        echo "File uploaded successfully ji!<br>";
+        echo "Download link: <a href='$fileUrl' target='_blank'>$fileUrl</a><br><br>";
+        echo "<h3>QR Code for Download Link:</h3>";
+        echo "<img src='$qrCodeUrl' alt='QR Code' style='border:1px solid black;padding:5px; border-radius: 15px;' >";
+        echo "<cite>By order of the peaky blinders";
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
