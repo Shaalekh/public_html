435c066dbcf0c49c16565e56fa8d5fb367768b77
Aalekh Kumar Sharma <shaalekh@gmail.com>
2024-11-21 21:10:23 +0530

    new commit

--- topm/receive.php
+++ topm/receive.php
@@ -1,5 +1,5 @@
 <?php
-$uniqueID = $_GET['uid'];
+$uniqueID = $_GET['uid'] ?? '';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,12 +13,23 @@ $uniqueID = $_GET['uid'];
             text-align: center;
             margin: 50px;
         }
+        .unique-id {
+            background-color: #f8f8f8;
+            border: 1px solid #ddd;
+            padding: 10px;
+            font-size: 18px;
+            display: inline-block;
+            margin: 20px auto;
+        }
     </style>
 </head>
 <body>
     <h1>Upload Files</h1>
+    <p>Your Unique ID (Save this for future reference):</p>
+    <div class="unique-id"><?php echo htmlspecialchars($uniqueID); ?></div>
+
     <form action="upload.php" method="POST" enctype="multipart/form-data">
-        <input type="hidden" name="uid" value="<?php echo $uniqueID; ?>">
+        <input type="hidden" name="uid" value="<?php echo htmlspecialchars($uniqueID); ?>">
         <input type="file" name="fileToUpload[]" multiple>
         <button type="submit">Upload</button>
     </form>
